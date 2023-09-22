a3a7ad0ab71e117c1555f68efa28164a69d65d49
kimjiseung97 <dickseung23@naver.com>
2023-09-22 23:56:28 +0900

    # 화면 테스트

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import BoardListItem from 'constants/BoradListItem';
 
 function App() {
   return (
     <>
+      <BoardListItem/>
     </>
   );
 }
