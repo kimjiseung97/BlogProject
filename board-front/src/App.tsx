990d01ce5b231126225ffd9b83dce0904b12e622
kimjiseung97 <dickseung23@naver.com>
2023-10-02 22:57:16 +0900

    # 아이콘 css 추가

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -6,14 +6,14 @@ import Top3Item from 'components/Top3Item';
 import CommentItem from 'components/CommentItem';
 import FavoriteItem from 'components/FavoriteItem';
 import InputBox from 'components/InputBox';
+import Footer from 'layouts/Fotter';
 
 function App() {
 
-  const[value,setValue] = useState<string>('');
 
   return (
     <>
-      <InputBox label='이메일'type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={false} setValue={setValue} message='aaa'/>
+      <Footer/>
     </>
   );
 }
